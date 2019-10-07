2f6098ce73b7666a6ad645f8e3b65ce5de5fc1fe
Pautov <pautovs@nail.ru>
2019-10-07 22:43:59 +0300

    Добавлено окно добавления типа ТНПА

--- TNPASerch/TNPASerch/View/AddType.xaml.cs
+++ TNPASerch/TNPASerch/View/AddType.xaml.cs
@@ -10,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using TNPASerch.View;
 
-namespace TNPASerch
+namespace TNPASerch.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AddType.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddType : Window
     {
-        public MainWindow()
+        public AddType()
         {
             InitializeComponent();
         }
-
     }
 }
