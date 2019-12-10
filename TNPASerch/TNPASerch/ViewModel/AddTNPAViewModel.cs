9f31a05f475fe3ad615a0edfc827de6a28e80a6f
Pautov <pautovs@nail.ru>
2019-12-10 22:53:27 +0300

    [+] Add View and ViewModel

---
+++ TNPASerch/TNPASerch/ViewModel/AddTNPAViewModel.cs
@@ -0,0 +1,27 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TNPASerch.View;
+
+namespace TNPASerch.ViewModel
+{
+    public class AddTNPAViewModel
+    {
+        public AddTNPAViewModel()
+        {
+            NewWindow = new RelayCommand(NewWindows);
+        }
+
+        private void NewWindows()
+        {
+            var asdl = new AddTNPAWindow();
+            asdl.Show();
+        }
+
+        public ICommand NewWindow { get; set; }
+    }
+}
