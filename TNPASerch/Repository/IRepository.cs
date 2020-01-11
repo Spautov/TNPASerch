a9c330438001fb8a35e4253fc603a62cc04dba3d
Pautov <pautovs@nail.ru>
2020-01-11 18:00:08 +0300

    [+] Добавлен проект Repository

---
+++ TNPASerch/Repository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+
+namespace Repository
+{
+    interface IRepository : IDisposable
+    {
+        IEnumerable<Tnpa> GetTnpaList();
+        IEnumerable<TnpaType> GetTnpaTypeList();
+        Tnpa GetTnpa(int id);
+        TnpaType GetTnpaType(int id);
+        void Create(Tnpa item);
+        void Create(TnpaType item);
+        void Update(Tnpa item);
+        void Update(TnpaType item);
+        void DeleteTnpa(int id);
+        void DeleteTnpaType(int id);
+        void Save();
+    }
+}
---
+++ TNPASerch/Repository/SQLiteRepository.cs
@@ -0,0 +1,78 @@
+﻿using DAL;
+using DbWorker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class SQLiteRepository : IRepository
+    {
+        private readonly TnpaDbContext _dbContext;
+        public SQLiteRepository()
+        {
+            _dbContext = new TnpaDbContext();
+        }
+        public void Create(Tnpa item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(TnpaType item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTnpa(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTnpaType(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tnpa GetTnpa(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Tnpa> GetTnpaList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TnpaType GetTnpaType(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TnpaType> GetTnpaTypeList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void Update(Tnpa item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TnpaType item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
