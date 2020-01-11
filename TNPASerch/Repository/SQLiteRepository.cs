759e7e2c0d0f9cc2168bd4b3604851b43272ea71
Pautov <pautovs@nail.ru>
2020-01-11 23:05:03 +0300

    [~] Реализация интерфейса IRepository

--- TNPASerch/Repository/SQLiteRepository.cs
+++ TNPASerch/Repository/SQLiteRepository.cs
@@ -1,5 +1,6 @@
 ﻿using DAL;
 using DbWorker;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,53 +12,76 @@ namespace Repository
     public class SQLiteRepository : IRepository
     {
         private readonly TnpaDbContext _dbContext;
-        public SQLiteRepository()
+
+        private static SQLiteRepository _SQLiteRepository;
+
+        public static SQLiteRepository GetRepository()
+        {
+            if (_SQLiteRepository == null)
+                _SQLiteRepository = new SQLiteRepository();
+            return _SQLiteRepository;
+        }
+
+        private SQLiteRepository()
         {
             _dbContext = new TnpaDbContext();
         }
         public void Create(Tnpa item)
         {
-            throw new NotImplementedException();
+            _dbContext.Tnpas.Add(item);
+            Save();
         }
 
         public void Create(TnpaType item)
         {
-            throw new NotImplementedException();
+            _dbContext.TnpaTypes.Add(item);
+            Save();
         }
 
         public void DeleteTnpa(int id)
         {
-            throw new NotImplementedException();
+            Tnpa tnpa = _dbContext.Tnpas.Find(id);
+            if (tnpa != null)
+                _dbContext.Tnpas.Remove(tnpa);
         }
 
         public void DeleteTnpaType(int id)
         {
-            throw new NotImplementedException();
-        }
-
-        public void Dispose()
-        {
-            throw new NotImplementedException();
+            TnpaType tnpaType = _dbContext.TnpaTypes.Find(id);
+            if (tnpaType != null)
+                _dbContext.TnpaTypes.Remove(tnpaType);
         }
 
         public Tnpa GetTnpa(int id)
         {
-            throw new NotImplementedException();
+            return _dbContext.Tnpas.Find(id);
         }
 
         public IEnumerable<Tnpa> GetTnpaList()
         {
-            throw new NotImplementedException();
+            return _dbContext.Tnpas;
+        }
+
+        async public ValueTask<IEnumerable<Tnpa>> GetTnpaListAsunc()
+        {
+            var resoult = await Task.Run(() => _dbContext.Tnpas);
+            return resoult;
         }
 
         public TnpaType GetTnpaType(int id)
         {
-            throw new NotImplementedException();
+            return _dbContext.TnpaTypes.Find(id);
+        }
+
+        async public ValueTask<IEnumerable<TnpaType>> GetTnpaTypeListAsunc()
+        {
+            var resoult = await Task.Run(() => _dbContext.TnpaTypes);
+            return resoult;
         }
 
         public IEnumerable<TnpaType> GetTnpaTypeList()
         {
-            throw new NotImplementedException();
+            return _dbContext.TnpaTypes;
         }
 
         public void Save()
@@ -67,12 +91,33 @@ namespace Repository
 
         public void Update(Tnpa item)
         {
-            throw new NotImplementedException();
+            _dbContext.Entry(item).State = EntityState.Modified;
+            Save();
         }
 
         public void Update(TnpaType item)
         {
-            throw new NotImplementedException();
+            _dbContext.Entry(item).State = EntityState.Modified;
+            Save();
+        }
+
+        private bool disposed = false;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _dbContext.Dispose();
+                }
+            }
+            this.disposed = true;
         }
     }
 }
