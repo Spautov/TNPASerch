c79f786f51044e2a394e02b8712eb171b3c80f8a
Pautov <pautovs@nail.ru>
2020-01-11 23:03:20 +0300

    [~] Добавлены новые моетоды интерфейса

--- TNPASerch/Repository/IRepository.cs
+++ TNPASerch/Repository/IRepository.cs
@@ -1,13 +1,16 @@
 ﻿using DAL;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Repository
 {
     interface IRepository : IDisposable
     {
         IEnumerable<Tnpa> GetTnpaList();
+        ValueTask<IEnumerable<Tnpa>> GetTnpaListAsunc();
         IEnumerable<TnpaType> GetTnpaTypeList();
+        ValueTask<IEnumerable<TnpaType>> GetTnpaTypeListAsunc();
         Tnpa GetTnpa(int id);
         TnpaType GetTnpaType(int id);
         void Create(Tnpa item);
