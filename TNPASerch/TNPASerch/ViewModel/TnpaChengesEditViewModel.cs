88193a7de05a75cb7652143aaa61c1c932c50cf0
Pautov <pautovs@nail.ru>
2020-01-26 21:43:49 +0300

    [~] Реализация редактора изменений

--- TNPASerch/TNPASerch/ViewModel/TnpaChengesEditViewModel.cs
+++ TNPASerch/TNPASerch/ViewModel/TnpaChengesEditViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DAL;
+using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,10 +18,60 @@ namespace TNPASerch.ViewModel
         public ICommand EditChangeCommand { get; set; }
         public ICommand CancelChangeCommand { get; set; }
 
+        private readonly ICollection<Change> _tnpaChanges;
 
-        public TnpaChengesEditViewModel(TnpaChengesEditView window) : base(window)
+        private ObservableCollection<Change> _changes;
+        public ObservableCollection<Change> Changes
         {
-                
+            get { return _changes; }
+            set
+            {
+                _changes = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Change _selectedChange;
+        public Change SelectedChange
+        {
+            get { return _selectedChange; }
+            set
+            {
+                _selectedChange = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public TnpaChengesEditViewModel(TnpaChengesEditView window, ICollection<Change> tnpaChanges) : base(window)
+        {
+            _tnpaChanges = tnpaChanges ?? throw new ArgumentNullException(nameof(tnpaChanges));
+
+            AddChangeCommand = new RelayCommand(AddChange);
+            RemoveChangeCommand = new RelayCommand(RemoveChange);
+            EditChangeCommand = new RelayCommand(EditChange);
+            CancelChangeCommand = new RelayCommand(Close);
+
+            GetChange();
+        }
+
+        private void EditChange()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RemoveChange()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddChange()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GetChange()
+        {
+            Changes = new ObservableCollection<Change>(_tnpaChanges);
         }
     }
 }
