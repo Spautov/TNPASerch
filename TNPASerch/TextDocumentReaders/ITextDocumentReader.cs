bbae00b983afe3a772e2781b2207f5222e1bdf91
Pautov <pautovs@nail.ru>
2020-03-18 22:51:21 +0300

    [+] Добавлена библиотека TextDocumentReaders для реализации функциона чтения контента из файлов pdf, doc, docx, txt

---
+++ TNPASerch/TextDocumentReaders/ITextDocumentReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextDocumentReaders
+{
+    public interface ITextDocumentReader
+    {
+        string GetContent(object filename);
+    }
+}
---
+++ TNPASerch/TextDocumentReaders/WordDocumentReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextDocumentReaders
+{
+    public class WordDocumentReader : ITextDocumentReader
+    {
+        public string GetContent(object filename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
