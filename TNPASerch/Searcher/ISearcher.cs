05c320cc72a3db93f1da77af99adbdb531e482cd
Pautov <pautovs@nail.ru>
2020-03-20 22:30:45 +0300

    [+] Добавление в проект функционала полнотекстового поиска на основе Lucene.Net

---
+++ TNPASerch/Searcher/ISearcher.cs
@@ -0,0 +1,14 @@
+﻿using DAL;
+using System.Collections.Generic;
+
+namespace Searcher
+{
+    public interface ISearcher
+    {
+        List<Tnpa> Serch();
+
+        void Add(Tnpa tnpa);
+
+        void Remove(Tnpa tnpa);
+    }
+}
---
+++ TNPASerch/Searcher/LuceneSercher.cs
@@ -0,0 +1,27 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searcher
+{
+    public class LuceneSercher : ISearcher
+    {
+        public void Add(Tnpa tnpa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Tnpa tnpa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tnpa> Serch()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
