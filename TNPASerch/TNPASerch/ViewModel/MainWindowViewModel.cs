58acf12f02ec5ac429b5c47b331be237034f060c
Pautov <pautovs@nail.ru>
2020-04-07 14:30:38 +0300

    [+] Добавлен конвертор дат

--- TNPASerch/TNPASerch/ViewModel/MainWindowViewModel.cs
+++ TNPASerch/TNPASerch/ViewModel/MainWindowViewModel.cs
@@ -8,7 +8,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Input;
 using TNPASerch.Model;
 using TNPASerch.View;
